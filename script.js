715b850671345c781088b26f19facce5b885a9c0
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-12-07 19:49:12 +0530

    Add : Markup, styling and JS for Custom Range Slider

---
+++ script.js
@@ -0,0 +1,7 @@
+const rangeSlider = document.querySelector("input");
+const valueEl = document.querySelector(".value");
+
+rangeSlider.oninput = function () {
+    valueEl.textContent = rangeSlider.value;
+    // valueEl.textContent = this.value;
+}
